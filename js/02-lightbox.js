f474d4c5072660e460fd88b4bfc2134af6340a9c
Nataly36 <movienata25@gmail.com>
2023-03-29 20:23:22 +0300

    vvv

---
+++ js/02-lightbox.js
@@ -0,0 +1,18 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+<div class="lightbox">
+    <div class="lightbox__overlay"></div>
+
+    <div class="lightbox__content">
+        <img class="lightbox___image " src="" alt="" />
+    </div>
+
+    <button
+        type="button"
+        class="lightbox__button"
+        data-action="close-lightbox"
+    >
+        <i class="material-icons">close</i>
+    </button>
+</div>
+console.log(galleryItems);
